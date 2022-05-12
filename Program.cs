09a941c42bcd8e01c39aeda2dbe8cc7537ab7c70
DaryaLatushka <100078700+DaryaLatushka@users.noreply.github.com>
2022-05-12 15:51:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Final
+{
+    static class Program
+    {
+
+        public static void Main()
+
+        {
+
+            const int lengthOfShortString = 3;
+
+            string[] sourceStrings = { "Sun", "Mond", "Tuesday",
+
+            "Wednesday", "Thursday", "Fri", "af", "s" };
+
+            int sourceStringsLength = sourceStrings.Length;
+
+            String[] shortStrings = new String[sourceStringsLength];
+
+            int shortStringsCount = 0;
+
+            for (int i = 0; i < sourceStrings.Length; i++)
+            {
+
+                if (sourceStrings[i].Length <= lengthOfShortString)
+
+                    shortStrings[shortStringsCount++] = sourceStrings[i];
+
+            }
+
+            Array.Resize(ref shortStrings, shortStringsCount);
+
+
+
+            for (int i = 0; i < shortStrings.Length; i++)
+            {
+
+                Console.WriteLine("Short string:[{0}] = {1}", i, shortStrings[i]);
+
+            }
+
+        }
+
+    }
+}
